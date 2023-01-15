c156e9940e7585a21a50b389d12b68bd151ead96
maksim ashmarin <maksimyou@gmail.com>
2023-01-15 06:59:01 +0300

    Моя первая сборка GULP

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+let gulp, { src, dest, series, parallel } = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+let cleanJS = require('gulp-uglify');
+let sass = require('gulp-sass')(require('sass'));
+let rename = require('gulp-rename');
+let autoprefixer = require('gulp-autoprefixer');
+let concat = require('gulp-concat');
+let del = require('del');
+let iconfont = require('gulp-iconfont');
+let imagemin = require('gulp-imagemin');
+let ttf2woff2 = require('gulp-ttf2woff2');
+// Объединяет все @media
+let cmq = require('gulp-group-css-media-queries');
+let fileinclude = require('gulp-file-include');
+
+
+
+
+
+
+
+//---------------------------------------------------Задачи---------------------------------------------------------
+
+
+//Объединение файлов html в одну страинцу 
+function taskHTML() {
+    return src('src/index.html')
+        .pipe(fileinclude())
+        .pipe(dest('dist'));
+};
+
+
+//Очищение папки dist
+function taskDel() {
+    return del('dist/*');
+};
+
+
+//Задачи над css файлами
+function taskCSS() {
+    return src('./src/scss/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            overrideBrowserslist: ["last 5 versions"]
+        }))
+        .pipe(cmq())
+        .pipe(cleanCSS())
+        .pipe(concat('bundle.css'))
+        .pipe(rename({
+            extname: '.min.css'
+        }))
+        .pipe(dest('dist/css'));
+};
+
+
+//Задачи над JS файлами
+function taskJS() {
+    return src('src/js/*.js')
+        .pipe(concat('bundle.js'))
+        .pipe(cleanJS())
+        .pipe(rename({
+            extname: '.min.js'
+        }))
+        .pipe(dest('dist/js'));
+};
+
+
+//Задачи над изображениями (минимизирует)
+
+function taskIMG() {
+    return src('src/img/**/*.{jpg,png,svg,gif,ico,webp}')
+        .pipe(imagemin())
+        .pipe(dest('dist/img'));
+};
+
+
+//Задачи над шрифтами (преобразует)
+
+function taskFonts() {
+    return src('src/fonts/*.ttf')
+        .pipe(ttf2woff2())
+        .pipe(dest('dist/fonts'));
+};
+
+
+
+exports.default = series(taskDel, parallel(taskCSS, taskJS, taskIMG, taskFonts, taskHTML));
