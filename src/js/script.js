5f38f69a0f94d4230c607a8464a5d22fdf423d34
maksim ashmarin <maksimyou@gmail.com>
2023-01-16 14:54:47 +0300

    Finis header work

--- src/js/script.js
+++ src/js/script.js
@@ -9,20 +9,32 @@ button_figure.addEventListener('mouseout', () => {
     document.querySelector('.get_consult').classList.toggle('active_a');
 })
 
+//Активация бургера
+
+let burgerHeader = document.querySelector('.burger_header');
+
+burgerHeader.addEventListener('click', () => {
+    burgerHeader.classList.toggle('active-b');
+    document.querySelector('.header_menu').classList.toggle('header_menu_show');
+});
+
+
+
+
 //Переброс элемента по точке останова
 
-let li = document.createElement('li');
+//let li = document.createElement('li');
 
-let headerPhone = document.querySelector('.header_phone');
+//let headerPhone = document.querySelector('.header_phone');
 
-window.addEventListener(('resize' || 'load'), (e) => {
-    if (window.outerWidth <= 520) {
-        let phone = headerPhone;
-        headerPhone.remove();
-        li.appendChild(phone);
-        document.querySelector('.header_menu_contant').appendChild(li);
-        e.preventDefault()
-    }
+//window.addEventListener(('resize' || 'onload'), (e) => {
+//    if (window.outerWidth <= 520) {
+//        let phone = headerPhone;
+//        headerPhone.remove();
+//        li.appendChild(phone);
+//        document.querySelector('.header_menu_contant').appendChild(li);
+//        e.preventDefault()
+//    }
 
-})
+//})
 
